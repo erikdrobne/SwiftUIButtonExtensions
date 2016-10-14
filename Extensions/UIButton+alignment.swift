fa207842bc90d24ccdca90cec939dc4662361bc0
Erik Drobne <erik.drobne@gmail.com>
2016-10-14 15:13:08 +0200

    Add auto layout support

--- Extensions/UIButton+alignment.swift
+++ Extensions/UIButton+alignment.swift
@@ -1,17 +1,17 @@
 import UIKit
 
 extension UIButton {
-    
+
     func verticalAlignImageAndTitle(padding: CGFloat = 5.0, isTitleUnderImage: Bool = true) {
         let imageSize = self.imageView!.frame.size
         let titleSize = self.titleLabel!.frame.size
-        
+
         if isTitleUnderImage {
-            self.titleEdgeInsets = UIEdgeInsetsMake(imageSize.height + padding, 0, 0, imageSize.width)
-            self.imageEdgeInsets = UIEdgeInsetsMake(0, titleSize.width, titleSize.height + padding, 0)
+            self.titleEdgeInsets = UIEdgeInsetsMake(0, -imageSize.width, -(imageSize.height + padding), 0)
+            self.imageEdgeInsets = UIEdgeInsetsMake(-(titleSize.height + padding), 0, 0, -titleSize.width)
         } else {
-            self.titleEdgeInsets = UIEdgeInsetsMake(0, 0, imageSize.height + padding, imageSize.width)
-            self.imageEdgeInsets = UIEdgeInsetsMake(titleSize.height + padding, titleSize.width, 0, 0)
+            self.titleEdgeInsets = UIEdgeInsetsMake(-(imageSize.height + padding), -imageSize.width, 0, 0)
+            self.imageEdgeInsets = UIEdgeInsetsMake(0, 0, -(titleSize.height + padding), -titleSize.width)
         }
     }
 }
